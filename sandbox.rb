afa381faadb12707c0aa147a9550a0c3b9a06602
sakunaga <sakunaga+github@nodepro.jp>
2024-09-10 10:22:25 +0900

    version up v0.0.3

--- sandbox.rb
+++ sandbox.rb
@@ -1,11 +1,10 @@
 class Sandbox < Formula
   homepage "https://github.com/sakunaga/sandbox"
   url "https://github.com/sakunaga/sandbox/archive/v0.0.2.tar.gz"
-  sha256 "c71c8fdfdcb22ca69d0c59233463a408c2ec6c525aacf1a9a794d6755f0cab8e"
+  sha256 "92d20af2f11e4d5d3e86a94bc41a43fa9dac941159176b30e85e629810af6ec8"
   license "MIT"
 
   def install
-    # system "./configure", "--disable-silent-rules", *std_configure_args
     bin.install "sandbox"
   end
 
