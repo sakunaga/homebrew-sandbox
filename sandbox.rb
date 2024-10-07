55955db7842895e86abbd2aaa86b4c25e314192a
sakunaga <sakunaga+github@nodepro.jp>
2024-10-08 00:56:51 +0900

    up

--- sandbox.rb
+++ sandbox.rb
@@ -1,7 +1,7 @@
 class sandbox < Formula
   homepage "https://github.com/sakunaga/sandbox"
-  url "https://github.com/sakunaga/sandbox/archive/v0.0.7.1.tar.gz"
-  sha256 "2b7d2e7a4382bc6b9108649d55c15fe3ff20ec6eb460a8c8108fb1c83916bca9"
+  url "https://github.com/sakunaga/sandbox/archive/v0.0.8.tar.gz"
+  sha256 "322b70dfd0e7401f54e4a80797e161cb12d14f320de39f14723910bf0b42c246"
   license "MIT"
 
   def install
